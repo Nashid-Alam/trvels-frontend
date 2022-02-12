07d1c0f708005f59512401f43b9ef205def8c69e
Nashid-Alam <nashid17@gmail.com>
2022-02-12 09:43:43 -0500

    added pictures to the cityCard

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -1,14 +1,14 @@
 import React from "react"
 
 function CityCard(props) {
-  console.log(props.city.posts[0])
+  
 
   return (
     <div>
       <h4 onClick={props.onClick}>
         {props.city.name}, {props.city.country}
       </h4>
-      {/* <img src={props.city.posts[0].pictures[1].url} alt="pics" />  */}
+      <img src={props.city.posts[0].pictures[0].url} alt="pics" /> 
     </div>
   )
 }
